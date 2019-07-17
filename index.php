e2fc47cc765616a36da2d740bb62065a383294c8
bayapucg <bayapu.edu004@gmail.com>
2019-07-17 16:56:44 +0530

    cimmitr
    fdfdasf

--- index.php
+++ index.php
@@ -29,6 +29,11 @@
 
  
 </head>
+<style>
+	.bg-f5f5f5{
+	background:#ddd !important;
+}
+</style>
 
 <body>
 
@@ -106,47 +111,7 @@
           <p>PERSONALISED ANDROID TABLETS ARE PLACED IN EACH HOTEL ROOM. HOTEL GUESTS CAN GET ALL THE NEEDED INFORMATION AND ORDER SERVICES WITH THE HELP OF TABLET IN WHICH THEY CAN FIND SPECIAL OFFERS, VISITOR ATTRACTIONS AND MENU IN THE MOST POPULAR LANGUAGES.</p>
         </header>
 
-        <div class="row about-cols">
-
-          <div class="col-md-4 wow fadeInUp">
-            <div class="about-col">
-              <div class="img">
-                <img src="img/about-mission.jpg" alt="" class="img-fluid">
-                <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
-              </div>
-              <h2 class="title"><a href="#">Our Mission</a></h2>
-              <p>
-                As a startup serving agency, we dig deep inside your brain and recognize your vision, mission and expectations from the business solution</p>
-            </div>
-          </div>
-
-          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
-            <div class="about-col">
-              <div class="img">
-                <img src="img/about-plan.jpg" alt="" class="img-fluid">
-                <div class="icon"><i class="ion-ios-list-outline"></i></div>
-              </div>
-              <h2 class="title"><a href="#">Our Plan</a></h2>
-              <p>
-                Realizing all the needs, we work towards developing secure, cleanly coded, mobile friendly, scalable and fully documented product (be it web or mobile)
-			  </p>
-            </div>
-          </div>
-
-          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
-            <div class="about-col">
-              <div class="img">
-                <img src="img/about-vision.jpg" alt="" class="img-fluid">
-                <div class="icon"><i class="ion-ios-eye-outline"></i></div>
-              </div>
-              <h2 class="title"><a href="#">Our Vision</a></h2>
-              <p>
-                We work continuously towards upgrading and enhancing your online product, adding latest features to keep it fresh and ever-evolving
-              </p>
-            </div>
-          </div>
-
-        </div>
+       
 
       </div>
     </section><!-- #about -->
@@ -161,6 +126,66 @@
           <h3>FEATURES</h3>
           <p>CO-OPERATIVE. TRANSPARENT. SUPPORTIVE. STRATEGIC</p>
 		  
+        </header>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-4 box wow bounceInUp" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-ios-analytics-outline"></i></div>
+            <h4 class="title"><a href="">Sub admin / Restaurant module:</a></h4>
+            <ul class="">
+				<li> 	Kitchen management</li>
+				<li>Menu management</li>
+				<li>Table management</li>
+				<li>Customer payments</li>
+				<li>Business analysis (orders/ revenue/ item sale) – daily, weekly, monthly.</li>
+				<li>Kitchen, Customer help & support</li>
+				<li>Offers & promotions</li>
+		    </ul>
+          </div>
+		    <div class="col-lg-4 col-md-4 box wow bounceInUp" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-ios-analytics-outline"></i></div>
+            <h4 class="title"><a href="">Kitchen</a></h4>
+            <ul class="">
+				<li>Table layout order updates</li>
+				<li>Update / close order</li>
+				
+		    </ul>
+          </div>
+		    <div class="col-lg-4 col-md-4 box wow bounceInUp" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-ios-analytics-outline"></i></div>
+            <h4 class="title"><a href="">Customer</a></h4>
+            <ul class="">
+				<li>Menu display</li>
+				<li>Order (multiple)</li>
+				<li>Feedback & Rating</li>
+				
+		    </ul>
+          </div>
+		</div>
+		<hr>
+		<div class="row">
+
+          <div class="col-lg-12 col-md-12 box wow bounceInUp" data-wow-duration="1.4s">
+            <ol class="">
+				<li>Cloud supported</li>
+				<li>Free updates </li>
+				<li>Integrated workflow</li>
+				<li>Improved Customer care</li>
+				<li>Graphical Data Representation</li>
+			
+		    </ol>
+          </div>
+	
+
+      </div>
+    </section>
+	  <section id="services" class="bg-f5f5f5">
+      <div class="container">
+
+        <header class="section-header wow fadeInUp">
+          <h3>Benefits</h3>
+        
         </header>
 
         <div class="row">
@@ -222,8 +247,7 @@
         </div>
 
       </div>
-    </section><!-- #services -->
-
+    </section>
     <!--==========================
       Call To Action Section
     ============================-->
